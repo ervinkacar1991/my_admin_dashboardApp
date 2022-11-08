823fe7e3eb7867df5aec98437869b702c68cb9fa
kacarervin <kacarervin@gmail.com>
2022-11-08 13:14:58 +0100

    Add users list

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import "./list.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        datatable
+      </div>
+    </div>
+  );
 };
 
 export default List;
